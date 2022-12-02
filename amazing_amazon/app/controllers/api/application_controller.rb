78faf39e140b0c2aef72286930ec24a29c578d3b
caleb.faulhaber@gmail.com <caleb.faulhaber@gmail.com>
2022-12-02 03:14:26 -0800

    amazon api: validation Errors & server errors

--- amazing_amazon/app/controllers/api/application_controller.rb
+++ amazing_amazon/app/controllers/api/application_controller.rb
@@ -3,6 +3,7 @@ class Api::ApplicationController < ApplicationController
   # $ rails g controller Api::Application --no-assets --no-helper --skip-template-engine
 
   skip_before_action :verify_authenticity_token
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
   def not_found
     render(
@@ -17,7 +18,21 @@ class Api::ApplicationController < ApplicationController
     )
   end
 
-
+  def record_invalid(error)
+    invalid_record = error.record
+    errors = invalid_record.errors.map do |field, message|
+      {
+        type: invalid_record.class.to_s,
+        field: field,
+        message: message,
+      }
+    end
+    render(
+      json: { status: 422, errors: errors },
+      status: 422,
+    )
+  end
+  
   private
 
   def authenticate_user!
--- amazing_amazon/app/controllers/api/v1/products_controller.rb
+++ amazing_amazon/app/controllers/api/v1/products_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::ProductsController < Api::ApplicationController
   before_action :find_product,only:[:show, :destroy, :update]
   before_action :authorize!, only: [:update, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
 
   # postman requests for testing
@@ -94,6 +94,22 @@ class Api::V1::ProductsController < Api::ApplicationController
   end
 
   protected
+
+  def record_invalid(error)
+    invalid_record = error.record
+    errors = invalid_record.errors.map do |errorObject|
+      {
+        type: error.class.to_s,
+        record_type: invalid_record.class.to_s,
+        field: errorObject.attribute,
+        message: errorObject.options[:message],
+      }
+    end
+    render(
+      json: { status: 422, errors: errors },
+      status: 422,
+    )
+  end
 
   def record_not_found(error)
     render(
